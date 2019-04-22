3cbc3047b83154c58a8d05da365e85964854e963
pd0220 <david.pesznyak@gmail.com>
2019-04-22 20:47:13 +0200

    harm osc test

--- main.cpp
+++ main.cpp
@@ -4,10 +4,21 @@
 //main function
 int main(int, char**)
 {
-    vector<double> v{1.,2.};
-    std::cout<<length(v)<<std::endl;
-    vector<double> w=norm(v);
-    std::cout<<w<<std::endl;
 
+    //harmonic oscillator
+    auto harm_osc=[&](double t,vector<double> s)->vector<double>{return {s[0],-s[1]};};
+
+    //initial condition
+    vector<double> y0{5.,5.};
+
+    //to file
+    auto to_file=[&](double t,vector<double> y){std::ofstream file;file.open("num_int.txt",std::fstream::app);file<<t<<" "<<y[0]<<" "<<y[1]<<"\n";file.close();};
+
+    //RK4
+    double t0=0.;
+    double t1=10.;
+    double h=1e-3;
+    solve_RK4(y0,t0,t1,h,harm_osc,to_file);
+    
     return 0;
 }
