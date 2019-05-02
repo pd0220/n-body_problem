022d91cc34d0cf7c88353ebab790009ee38e7c14
pd0220 <david.pesznyak@gmail.com>
2019-05-02 23:45:35 +0200

    adaptive test

--- main.cpp
+++ main.cpp
@@ -93,7 +93,7 @@ int main(int, char**)
     double t0=0.;
     double t1=1e9;
     double h=1e5;
-    double delta_0=1e3;
+    double delta_0=1e4;
     //solve_RK4_adapt(y0,t0,t1,h,armageddon,to_file,delta_0);
     solve_RK4(y0,t0,t1,h,armageddon,to_file);
 
